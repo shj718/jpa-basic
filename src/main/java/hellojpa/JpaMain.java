aeffb5dc125e669f3a732b35323411f11fd46246
hyejunseo <shyejun0718@naver.com>
2022-06-07 21:45:56 +0900

    JPQL 연습

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -2,15 +2,37 @@ package hellojpa;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
 
     public static void main(String[] args) {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-        EntityManager entityManager = emf.createEntityManager();
-        // TODO: DB 작업 (데이터 저장 등)
-        entityManager.close();
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello"); // 애플리케이션 로딩 시점에 딱 1개
+
+        EntityManager entityManager = emf.createEntityManager(); // 일련의 DB 작업을 할 때마다 생성 (Ex. 장바구니에 물건 추가)
+
+        // 트랜잭션 필수
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+
+        // DB 작업 (데이터 저장 등)
+        try {
+            List<Member> members = entityManager.createQuery("select m from Member as m where m.id < 3L", Member.class)
+                    .getResultList();
+            for(Member member : members) {
+                System.out.println("member.name = " + member.getName());
+            }
+
+            // 커밋
+            transaction.commit();
+        } catch (Exception e) {
+            transaction.rollback();
+        } finally {
+            entityManager.close(); // 항상 잘 닫아주기 (DB 커넥션을 물고 있음)
+        }
+
         emf.close();
     }
 }
