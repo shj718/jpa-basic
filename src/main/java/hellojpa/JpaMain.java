6b69d83ed1c6471acc3cf9ab8195cae70f8761c6
hyejunseo <shyejun0718@naver.com>
2022-06-10 21:16:32 +0900

    PK 매핑

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -11,26 +11,24 @@ public class JpaMain {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello"); // 애플리케이션 로딩 시점에 딱 1개
 
-        EntityManager entityManager = emf.createEntityManager(); // 일련의 DB 작업을 할 때마다 생성 (Ex. 장바구니에 물건 추가)
+        EntityManager em = emf.createEntityManager(); // 일련의 DB 작업을 할 때마다 생성 (Ex. 장바구니에 물건 추가)
 
         // 트랜잭션 필수
-        EntityTransaction transaction = entityManager.getTransaction();
+        EntityTransaction transaction = em.getTransaction();
         transaction.begin();
 
         // DB 작업 (데이터 저장 등)
         try {
             Member member = new Member();
-            member.setId(1L);
-            member.setUsername("A");
-            member.setRoleType(RoleType.USER);
+            member.setUsername("B");
+            em.persist(member);
 
-            entityManager.persist(member);
             // 커밋
             transaction.commit();
         } catch (Exception e) {
             transaction.rollback();
         } finally {
-            entityManager.close(); // 항상 잘 닫아주기 (DB 커넥션을 물고 있음)
+            em.close(); // 항상 잘 닫아주기 (DB 커넥션을 물고 있음)
         }
 
         emf.close();
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,32 +1,17 @@
 package hellojpa;
 
 import javax.persistence.*;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Date;
 
 @Entity
+@SequenceGenerator(name = "MEMBER_SEQ_GENERATOR", sequenceName = "MEMBER_SEQ", initialValue = 1, allocationSize = 1)
 public class Member {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MEMBER_SEQ_GENERATOR")
     private Long id;
+
     @Column(name = "name", nullable = false)
     private String username;
-    private Integer age;
-    @Enumerated(EnumType.STRING)
-    private RoleType roleType;
-
-    // 최근에는 사용하지 않음. LocalDate, LocalDateTime 으로 대체 가능
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date createdDate;
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date lastModifiedDate;
-    // 대체
-    private LocalDate localDateTest;
-    private LocalDateTime localDateTimeTest;
-
-    @Lob
-    private String description;
 
     // JPA 는 기본 생성자가 필수.
     public Member() {
@@ -47,61 +32,5 @@ public class Member {
 
     public void setUsername(String username) {
         this.username = username;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public RoleType getRoleType() {
-        return roleType;
-    }
-
-    public void setRoleType(RoleType roleType) {
-        this.roleType = roleType;
-    }
-
-    public Date getCreatedDate() {
-        return createdDate;
-    }
-
-    public void setCreatedDate(Date createdDate) {
-        this.createdDate = createdDate;
-    }
-
-    public Date getLastModifiedDate() {
-        return lastModifiedDate;
-    }
-
-    public void setLastModifiedDate(Date lastModifiedDate) {
-        this.lastModifiedDate = lastModifiedDate;
-    }
-
-    public LocalDate getLocalDateTest() {
-        return localDateTest;
-    }
-
-    public void setLocalDateTest(LocalDate localDateTest) {
-        this.localDateTest = localDateTest;
-    }
-
-    public LocalDateTime getLocalDateTimeTest() {
-        return localDateTimeTest;
-    }
-
-    public void setLocalDateTimeTest(LocalDateTime localDateTimeTest) {
-        this.localDateTimeTest = localDateTimeTest;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
     }
 }
